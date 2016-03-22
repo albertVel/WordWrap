cfa6a281976473ab6d1c89d5841a350679bd4b99
albertVel <xumiii@gmail.com>
2016-03-22 23:59:54 +0100

    Fixed test

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -12,7 +12,7 @@ namespace WordWrapNamespace
         {
             var temp = text;
 
-            if (width < text.Length)
+            if (this.TextDoNotFit(text, width))
             {
                 var cuttingPoint = text.LastIndexOf(" ");
                 if (cuttingPoint != -1)
@@ -21,9 +21,25 @@ namespace WordWrapNamespace
                     var remainingText = text.Substring(0, cuttingPoint);
                     return this.WordWrapString(remainingText, width) + "\n" + wrappedText;
                 }
+                else
+                {
+                    if (this.TextDoNotFit(text, width))
+                    {
+                        var cuttingPointTextNoSpaces = text.Length - width;
+
+                        var wrappedText = text.Substring(cuttingPointTextNoSpaces).Trim();
+                        var remainingText = text.Substring(0, cuttingPointTextNoSpaces);
+                        return this.WordWrapString(remainingText, width) + "\n" + wrappedText;
+                    }
+                }
             }
 
             return temp;
+        }
+
+        private bool TextDoNotFit(string text, int width)
+        {
+            return width < text.Length;
         }
     }
 }
