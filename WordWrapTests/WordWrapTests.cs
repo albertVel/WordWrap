4e4eb5a3ef666162884272a1931501ea592b24da
albertVel <xumiii@gmail.com>
2016-03-22 22:18:54 +0100

    Renamed test class. Added first test, it fails.

---
+++ WordWrapTests/WordWrapTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap
+{
+    using FluentAssertions;
+
+    [TestClass]
+    public class WordWrapTests
+    {
+        [TestMethod]
+        public void Given_A_String_Without_Spaces_WordWrap_Should_Return_Same_String()
+        {
+            var result = WordWrapString("hello", 8);
+
+            result.Should().Be("hello");
+        }
+
+        private object WordWrapString(string text, int width)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
