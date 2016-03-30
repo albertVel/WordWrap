7e3aa7ecf15dfb24ab92804420500f9a4ba7d336
albertVel <xumiii@gmail.com>
2016-03-30 22:47:48 +0200

    Added test to check behavior of wordwrap when matching width and space

--- WordWrapTests/WordWrapTests.cs
+++ WordWrapTests/WordWrapTests.cs
@@ -64,5 +64,14 @@ namespace WordWrapTestsNamespace
 
             result.Should().Be("This sentence\nshould be splitted\nseveral times");
         }
+
+        [TestMethod]
+        public void Given_A_String_With_Several_Spaces_Set_Width_Equals_To_Space_Return_String_Splitted_And_Trimmed()
+        {
+            WordWrap wordWrap = new WordWrap();
+            var result = wordWrap.WordWrapString("TestTheBehavior of wordwrap when matching width and space", 15);
+
+            result.Should().Be("TestTheBehavior\nof wordwrap\nwhen matching\nwidth and space");
+        }
     }
 }
